07e422dc8113eb5b12f0edfc0105ccc94375782e
shivammpce <shivammpce@gmail.com>
2024-04-09 21:46:26 +0530

    added switch case

---
+++ switch_statements/main.go
@@ -0,0 +1,15 @@
+package main
+
+// in switch case we take a variable and checks for possible values
+func main() {
+	number := 3
+
+	switch number {
+	case 1:
+		println("Number is 1")
+	case 2:
+		println("Number is 2")
+	case 3:
+		println("Number is 3")
+	}
+}
