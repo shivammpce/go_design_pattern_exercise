09801271aba3105fa30e5189c449add37e205cb7
shivammpce <shivammpce@gmail.com>
2024-04-10 23:47:18 +0530

    added for anonymous func

---
+++ anonymous_function/main.go
@@ -0,0 +1,12 @@
+package main
+
+// An anonymous function is a function without a name. This is useful when we want to return
+// a function from another function that doesn't need a context or when we want to pass a function
+// to a different function
+
+func main() {
+	add := func(m int)retuen {
+		return
+	}
+	result := add(6)
+}
