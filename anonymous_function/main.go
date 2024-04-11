01adc8e5a0dc3b93dac90f9e829c6e51d9624a21
shivammpce <shivammpce@gmail.com>
2024-04-11 23:20:39 +0530

    added anonymous addition func

--- anonymous_function/main.go
+++ anonymous_function/main.go
@@ -1,12 +1,15 @@
 package main
 
+import "fmt"
+
 // An anonymous function is a function without a name. This is useful when we want to return
 // a function from another function that doesn't need a context or when we want to pass a function
 // to a different function
 
 func main() {
-	add := func(m int)retuen {
-		return
+	add := func(m int) int {
+		return m + 1
 	}
 	result := add(6)
+	fmt.Printf("result: %v\n", result)
 }
