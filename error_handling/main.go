5c84198f206417a2a6a4b80ab73efaee81f18b06
shivammpce <shivammpce@gmail.com>
2024-04-12 23:04:10 +0530

    added error handling

--- error_handling/main.go
+++ error_handling/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+// to create an error simply make a call to error.New(string).
+func main() {
+	err := errors.New("Error created")
+	fmt.Println(err)
 }
