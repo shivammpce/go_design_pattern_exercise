452249a15f295486f5f2054b0a77236deb3a4d29
shivammpce <shivammpce@gmail.com>
2024-04-12 23:19:52 +0530

    added error handling

--- error_handling/main.go
+++ error_handling/main.go
@@ -7,6 +7,20 @@ import (
 
 // to create an error simply make a call to error.New(string).
 func main() {
+	err := doesReturnError1()
+	if err != nil {
+		fmt.Printf("err1: %v\n", err)
+	}
+	err2 := doesReturnError2(errors.New("Error created"))
+	if err2 != nil {
+		fmt.Printf("err2: %v\n", err2.Error())
+	}
+}
+
+func doesReturnError1() error {
 	err := errors.New("Error created")
-	fmt.Println(err)
+	return err
+}
+func doesReturnError2(err error) error {
+	return err
 }
